7b8ac568dd80cc269ed942254bcf56e2c375a1a2
JL-OLEMAR <joseluis19963@gmail.com>
2021-11-13 15:11:12 -0500

    configurando cors

--- index.ts
+++ index.ts
@@ -1,15 +1,20 @@
 import bodyParser from 'body-parser'
+import cors from 'cors'
+
 import Server from './class/server'
 import router from './routes/router'
 
 // Instancia del servidor
 const server = new Server()
 
-// Body parser
+// Middlewares
 server.app.use(bodyParser.urlencoded({ extended: true }))
 server.app.use(bodyParser.json())
 
-// Router
+// Cors
+server.app.use(cors({ origin: true, credentials: true }))
+
+// Routes
 server.app.use('/', router)
 
 // Levantar el servidor
