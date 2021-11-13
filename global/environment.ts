f30fc4c0883a589e1caf89595b734ddec56b0b55
JL-OLEMAR <joseluis19963@gmail.com>
2021-11-13 02:14:24 -0500

    project init

---
+++ global/environment.ts
@@ -0,0 +1,2 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+export const SERVER_PORT: number = Number(process.env.PORT) || 5000
---
+++ index.ts
@@ -0,0 +1,7 @@
+import Server from './class/server'
+
+const server = new Server()
+
+server.start(() => {
+  console.log(`Servidor corriendo en http://localhost:${server.port}`)
+})
