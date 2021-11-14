a88affa182bd727e660bba56a22f41a93a94a2f0
JL-OLEMAR <joseluis19963@gmail.com>
2021-11-13 21:07:03 -0500

    inicializando las conexiones de sockets

--- class/server.ts
+++ class/server.ts
@@ -1,16 +1,32 @@
 import express from 'express'
+import http from 'http'
+import socketIO from 'socket.io'
 import { SERVER_PORT } from '../global/environment'
 
 export default class Server {
   public app: express.Application
   public port: number
+  public io: socketIO.Server
+  private readonly httpServer: http.Server
 
   constructor () {
     this.app = express()
     this.port = SERVER_PORT
+    this.httpServer = new http.Server(this.app)
+    this.io = new socketIO.Server(this.httpServer, { cors: { origin: '*' } })
+    this.escucharSockets()
+  }
+
+  // Escuchar conexiones de sockets
+  private escucharSockets (): void {
+    console.log('Escuchando conexiones - sockets')
+    // Conectar cliente
+    this.io.on('connection', cliente => {
+      console.log('Cliente conectado')
+    })
   }
 
   start (callback: Function): void {
-    this.app.listen(this.port, callback())
+    this.httpServer.listen(this.port, callback())
   }
 }
