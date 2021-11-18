8e9d4ca47bc3788f7a973f8b6de4b8e712020ffb
JL-OLEMAR <joseluis19963@gmail.com>
2021-11-18 00:03:09 -0500

    add nuevo metodo, que escucha el evento configurar-usuario

--- class/server.ts
+++ class/server.ts
@@ -26,9 +26,9 @@ export default class Server {
     return this._instance || (this._instance = new this())
   }
 
-  // Escuchar conexiones de sockets
+  // Escuchar conexiones emitidas por el frontend
   private escucharSockets (): void {
-    console.log('🧏‍♂️ Escuchando conexiones - ⚡sockets')
+    console.log('🧏‍♂️ Escuchando todas las conexiones - ⚡sockets')
     // Conectar cliente con el servidor
     this.io.on('connection', cliente => {
       console.log('✅ Cliente conectado')
@@ -38,6 +38,9 @@ export default class Server {
 
       // Desconexion del cliente
       socket.desconectar(cliente)
+
+      // Configurar usuario
+      socket.configurandoUsuario(cliente, this.io)
     })
   }
 
--- sockets/socket.ts
+++ sockets/socket.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/explicit-function-return-type, standard/no-callback-literal */
 import socketIO, { Socket } from 'socket.io'
 
 // Metodo para desconexion de cliente del socket
@@ -16,5 +16,19 @@ export const mensaje = (cliente: Socket, io: socketIO.Server) => {
 
     // Emitir mensaje a todos los clientes
     io.emit('mensaje-nuevo', payload)
+  })
+}
+
+// Escuchar evento, Configurar Usuario
+export const configurandoUsuario = (cliente: Socket, io: socketIO.Server) => {
+  // Escuchar mensaje de cliente
+  cliente.on('configurar-usuario', (payload: { nombre: string }, callback: Function) => {
+    console.log('🧑‍💻 Configurando Usuario:', payload.nombre)
+
+    // Mandar el callback al cliente, con los datos del usuario
+    callback({
+      ok: true,
+      mensaje: `El usuario: ${payload.nombre} esta configurado`
+    })
   })
 }
