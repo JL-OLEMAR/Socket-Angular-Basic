993be7d19cc404a9de91b1fa390481a0569682ec
JL-OLEMAR <joseluis19963@gmail.com>
2021-11-21 13:25:52 -0500

    Emitir mensajes a todos los usuarios

--- routes/router.ts
+++ routes/router.ts
@@ -16,6 +16,10 @@ router.post('/mensajes', (req: Request, res: Response) => {
   const cuerpo = req.body.cuerpo
   const de = req.body.de
 
+  const payload = { cuerpo, de }
+  const server = Server.instance
+  server.io.emit('mensaje-nuevo', payload)
+
   res.json({
     ok: true,
     cuerpo,
