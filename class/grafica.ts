abecebf920b7a0a9553209ad3c75bbc1b5ecd388
JL-OLEMAR <joseluis19963@gmail.com>
2021-11-23 12:17:04 -0500

    add la clase grafica para controlar la grafica

---
+++ class/grafica.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-for-in-array */
+export class GraficaData {
+  // meses del año
+  private readonly meses: string[] = ['january', 'february', 'march', 'april']
+  private readonly valores: number[] = [1, 2, 3, 4]
+
+  // Mostrar datos de la grafica
+  getDataGrafica (): any {
+    return [{ data: this.valores, label: 'Ventas' }]
+  }
+
+  // Incrementar valor de la grafica
+  incrementarValor (mes: string, valor: number): any {
+    mes = mes.toLowerCase().trim()
+    for (const i in this.meses) {
+      if (this.meses[i] === mes) {
+        this.valores[i] += valor
+      }
+    }
+    return this.getDataGrafica()
+  }
+}
