6e26c2ec5bb36b207a594dd87ca75cb28757eef6
JL-OLEMAR <joseluis19963@gmail.com>
2021-11-26 03:14:48 -0500

    Add new ruta para el mapa

--- routes/router.ts
+++ routes/router.ts
@@ -1,37 +1,30 @@
 import { Router, Request, Response } from 'express'
 import Server from '../class/server'
 import { usuariosConectados } from '../sockets/socket'
-import { GraficaData } from '../class/grafica'
 import { GraficaBarraData } from '../class/graficaBarras'
+import { GraficaData } from '../class/grafica'
+import { Mapa } from '../class/mapa'
 
 const router = Router()
-
 const grafica = new GraficaData()
 const graficaBarras = new GraficaBarraData()
+const mapa = new Mapa()
 
-// GET /grafica-barras - Devuelve data de la grafica en barras
-router.get('/grafica-barras', (req: Request, res: Response) => {
-  res.json(graficaBarras.getDataGraficaBarra())
+// ---------------------MAPAS--------------------------------------------------------
+
+// Devuelve data del mapa
+router.get('/mapa', (req: Request, res: Response) => {
+  res.json(mapa.getMarcadores())
 })
 
-// POST /grafica-barras - Incrementa unidades de la grafica segun la opcion de la pregunta
-router.post('/grafica-barras', (req: Request, res: Response) => {
-  const opcion = Number(req.body.opcion)
-  const unidades = Number(req.body.unidades)
-  graficaBarras.incrementarUnidades(opcion, unidades)
+// ---------------------GRAFICAS-----------------------------------------------------
 
-  const server = Server.instance
-  server.io.emit('cambio-grafica-barras', graficaBarras.getDataGraficaBarra())
-
-  res.json(graficaBarras.getDataGraficaBarra())
-})
-
-// GET /grafica - Devuelve data de la grafica
+// Devuelve data de la grafica
 router.get('/grafica', (req: Request, res: Response) => {
   res.json(grafica.getDataGrafica())
 })
 
-// POST /grafica - Incrementa unidades de la grafica segun la opcion de la pregunta
+// Incrementa unidades de la grafica segun la opcion de la pregunta
 router.post('/grafica', (req: Request, res: Response) => {
   const mes = req.body.mes
   const unidades = Number(req.body.unidades)
@@ -45,7 +38,28 @@ router.post('/grafica', (req: Request, res: Response) => {
   res.json(grafica.getDataGrafica())
 })
 
-// GET /mensajes - Devuelve todos los mensajes
+// ---------------------GRAFICAS EN BARRAS-------------------------------------------
+
+// Devuelve data de la grafica en barras
+router.get('/grafica-barras', (req: Request, res: Response) => {
+  res.json(graficaBarras.getDataGraficaBarra())
+})
+
+// Incrementa unidades de la grafica segun la opcion de la pregunta
+router.post('/grafica-barras', (req: Request, res: Response) => {
+  const opcion = Number(req.body.opcion)
+  const unidades = Number(req.body.unidades)
+  graficaBarras.incrementarUnidades(opcion, unidades)
+
+  const server = Server.instance
+  server.io.emit('cambio-grafica-barras', graficaBarras.getDataGraficaBarra())
+
+  res.json(graficaBarras.getDataGraficaBarra())
+})
+
+// ---------------------MENSAJES-----------------------------------------------------
+
+// Devuelve todos los mensajes
 router.get('/mensajes', (req: Request, res: Response) => {
   res.json({
     ok: true,
@@ -53,7 +67,7 @@ router.get('/mensajes', (req: Request, res: Response) => {
   })
 })
 
-// POST /mensajes - Crea un nuevo mensaje
+// Crea un nuevo mensaje
 router.post('/mensajes', (req: Request, res: Response) => {
   const cuerpo = req.body.cuerpo
   const de = req.body.de
@@ -69,7 +83,7 @@ router.post('/mensajes', (req: Request, res: Response) => {
   })
 })
 
-// POST /mensajes/:id - Actualiza un mensaje
+// Actualiza un mensaje
 router.post('/mensajes/:id', (req: Request, res: Response) => {
   const de = req.body.de
   const cuerpo = req.body.cuerpo
@@ -88,7 +102,9 @@ router.post('/mensajes/:id', (req: Request, res: Response) => {
   })
 })
 
-// GET, Servicio para obtener todos los IDs de los usuarios
+// ---------------------USUARIOS-----------------------------------------------------
+
+// Servicio para obtener todos los IDs de los usuarios
 router.get('/usuarios', (req: Request, res: Response) => {
   const server = Server.instance
 
