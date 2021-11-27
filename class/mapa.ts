748ed00ff9a6a0aba15ebeb88c5b3027c3377127
JL-OLEMAR <joseluis19963@gmail.com>
2021-11-26 19:15:30 -0500

    add metodo para agregar marcador

--- class/mapa.ts
+++ class/mapa.ts
@@ -3,35 +3,18 @@ import { Marcador } from './marcador'
 
 export class Mapa {
   // Propiedad marcadores de tipo objeto, que contendra un array de Marcador
-  private readonly marcadores: {[key: string]: Marcador} = {
-    1: {
-      id: '1',
-      nombre: 'Fernando',
-      lng: -75.75512993582937,
-      lat: 45.349977429009954,
-      color: '#dd8fee'
-    },
-    2: {
-      id: '2',
-      nombre: 'Amy',
-      lng: -75.75195645527508,
-      lat: 45.351584045823756,
-      color: '#790af0'
-    },
-    3: {
-      id: '3',
-      nombre: 'Orlando',
-      lng: -75.75900589557777,
-      lat: 45.34794635758547,
-      color: '#19884b'
-    }
-  }
+  private readonly marcadores: {[key: string]: Marcador} = {}
 
   // constructor() {}
 
   // getter para obtener el array de marcadores
   getMarcadores (): {[key: string]: Marcador} {
     return this.marcadores
+  }
+
+  // Metodo para agregar un marcador al mapa
+  agregarMarcador (marcador: Marcador): void {
+    this.marcadores[marcador.id] = marcador
   }
 
   // Metodo para botar un marcador en el mapa
